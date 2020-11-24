caec5292e2e67a737fa9ca60bfd94c632a868414
Charles Shen <ssa19990208@foxmail.com>
2020-11-24 11:45:59 +0800

    FSK_demodulation

--- src/FSK.py
+++ src/FSK.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy import signal
-
+from utils import load_wave, save_wave, init_args, generate_random_seq, compare_seqs
 
 def generate_pulse(framerate, frequency, volume, start_place, duration):
     '''
@@ -17,7 +17,7 @@ def modulation(args, bits):
     '''
     描述：FSK调制算法
     参数：全局参数, 0-1比特信号（规定比特信号长度小于等于一个包的限制）
-    返回：得到的波
+    返回：得到的波---numpy格式的一维数组
     '''
     result_wave = np.empty(shape = (1, 0))
     for bit in bits:
@@ -29,6 +29,59 @@ def modulation(args, bits):
         result_wave = np.append(result_wave, y)
     return result_wave
 
+def get_dominate_frequency(args, wave):
+    '''
+    描述：获取一个区间内主导性频率
+    参数：全局参数，区间的波形（numpy格式一维数组）
+    返回：主导频率
+    '''
+    fourier_result = abs(np.fft.fft(wave))
+    index = np.argmax(fourier_result)
+    frequency = round(index / len(fourier_result) * args.framerate)
+    return frequency
 
+def demodulation(args, wave):
+    '''
+    描述：FSK解调算法
+    参数：全局参数，波---numpy格式的一维数组
+    返回：0-1比特信号
+    '''
+    result = []
+    window_length = round(args.framerate * args.window_length)
+    threshold = 0.1 #如果窗口太小了就不考虑了
+    frequency_range = [0.9, 1.1]
+    start_place = 0
+    while start_place < len(wave):
+        if start_place + window_length <= len(wave):
+            length = window_length
+        else: 
+            length = window_length - start_place
+        if length < window_length * threshold:
+            continue
+        
+        sub_wave = wave[start_place: start_place + length - 1]
+        dominate_frequency = get_dominate_frequency(args, sub_wave)
+        if dominate_frequency >= args.frequency_0 * frequency_range[0] and dominate_frequency <= args.frequency_0 * frequency_range[1]:
+            result.append(0)
+        elif dominate_frequency >= args.frequency_1 * frequency_range[0] and dominate_frequency <= args.frequency_1 * frequency_range[1]:
+            result.append(1)
+        start_place += window_length
+    return result
 
+if __name__ == '__main__':
+    args = init_args()
+    original_seq = generate_random_seq(300)
+    print("The original seq is:\n", original_seq)
 
+    the_wave = modulation(args, original_seq)
+    save_wave(the_wave, framerate = args.framerate, sample_width = args.sample_width, nchannels = args.nchannels, save_base = 'send', file_name = 'kebab.wav')
+    get_wave = load_wave(save_base = 'send', file_name = 'kebab.wav')
+    get_seq = demodulation(args, get_wave)
+    print("The loaded seq is:\n", get_seq)
+
+    result = compare_seqs(original_seq, get_seq)
+    if result:
+        print("The original seq and the seq I get is identical, right!")
+    else: 
+        print("The original seq and the seq I get is not identical, wrong!")
+    
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,12 @@
+import wave
+import os
+import numpy as np
+import random
+from scipy.io import wavfile
+import struct
+import matplotlib.pyplot as plt
+from scipy import signal
 import argparse
-
 
 def init_args():
     '''
@@ -17,7 +24,7 @@ def init_args():
     parser.add_argument("--framerate", type = int, default = 48000)
     parser.add_argument("--sample_width", type = int, default = 2)
     parser.add_argument("--nchannels", type = int, default = 1)
-    parser.add_argument("--volume", type = float, default = 10000.0)
+    parser.add_argument("--volume", type = float, default = 1000.0)
     parser.add_argument("--start_place", type = int, default = 0)
 
     #单个窗口的长度(单位秒)
@@ -31,3 +38,69 @@ def init_args():
     parser.add_argument("--save_base_receive", type = str, default = 'receive')
     args = parser.parse_args()
     return args
+
+def save_wave(my_wave, framerate = 44100, sample_width = 2, nchannels = 1, save_base = 'sound', file_name = 'pulse.wav'):
+    '''
+    描述：存储wav文件
+    参数：波，帧率，采样宽度， 通道数，文件夹名，文件名
+    返回：无
+    '''
+    the_place = os.path.join(save_base, file_name)
+    wf = wave.open(the_place, 'wb')
+    wf.setnchannels(nchannels)
+    wf.setframerate(framerate)
+    wf.setsampwidth(sample_width)
+    for i in range(len(my_wave)):
+        the_result = int(my_wave[i]) 
+        data = struct.pack('<h', the_result)
+        wf.writeframesraw(data)
+    wf.close()
+
+
+def load_wave(save_base = 'sound', file_name = 'pulse.wav'):
+    '''
+    描述：读取wav文件
+    参数：文件夹名，文件名, 帧数，帧率，采样时间，长度
+    返回：y
+    '''
+    the_place = os.path.join(save_base, file_name)
+    file = wave.open(the_place)
+    n_frames = file.getparams().nframes  # 帧总数
+    framerate = file.getparams().framerate  # 采样频率
+    sample_time = 1 / framerate  # 采样点的时间间隔
+    duration = n_frames / framerate  # 声音信号的长度
+
+    frequency, audio_sequence = wavfile.read(the_place)
+    x_seq = np.arange(0, duration, sample_time)
+    print("n_frames:", n_frames)
+    print("framerate:", framerate)
+    print("sample_time:", sample_time)
+    print("duration:", duration)
+    print("frequency:", frequency)
+    y = audio_sequence  
+    return y
+
+def generate_random_seq(length):
+    '''
+    描述：生成随机0-1序列
+    参数：长度
+    返回：随机0-1序列
+    '''
+    original_seq = []
+    for i in range(length):
+        num = round(random.random())
+        original_seq.append(num)
+    return original_seq
+
+def compare_seqs(original_seq, get_seq):
+    '''
+    描述：比较两个seq是否一样
+    参数：原先和获取的seq
+    返回：正确/错误
+    '''
+    if len(original_seq) != len(get_seq):
+        return False
+    for i in range(len(original_seq)):
+        if original_seq[i] != get_seq[i]:
+            return False
+    return True
