6b09c8251c93b1d339966a9ae630b04320ccc69d
Rhacoal <rhacoal@gmail.com>
2020-12-06 17:10:22 +0800

    new algorithm for fsk demodulation

--- src/FSK.py
+++ src/FSK.py
@@ -2,30 +2,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
 from utils import *
-import statsmodels.tsa.api as smt 
+import statsmodels.tsa.api as smt
+
 
 def generate_pulse(framerate, frequency, volume, start_place, duration):
-    '''
+    """
     描述：生成脉冲信号
     参数：帧率，频率，振幅，相位，时长
     返回：波形
-    '''
+    """
     n_frames = round(duration * framerate)
-    x = np.linspace(0, duration, num = n_frames)
+    x = np.linspace(0, duration, num=n_frames)
     y = np.sin(2 * np.pi * frequency * x + start_place) * volume
     return y
 
+
 def modulation(args, bits):
-    '''
+    """
     描述：FSK调制算法
     参数：全局参数, 0-1比特信号（规定比特信号长度小于等于一个包的限制）
     返回：得到的波---numpy格式的一维数组
-    '''
-    result_wave = np.empty(shape = (1, 0))
+    """
+    result_wave = np.empty(shape=(1, 0))
     for bit in bits:
         if bit == 0:
             frequency = args.frequency_0
-        else: 
+        else:
             frequency = args.frequency_1
         y = generate_pulse(args.framerate, frequency, args.volume, args.start_place, args.window_length)
         result_wave = np.append(result_wave, y)
@@ -33,11 +35,11 @@ def modulation(args, bits):
 
 
 def get_window_start(args, wave):
-    '''
+    """
     描述：用前导码对齐窗口
     参数：全局参数，波---numpy格式的一维数组
     返回：前导码在哪？
-    '''
+    """
     preamble_seq = modulation(args, args.preamble)
     length = len(wave)
     length_preamble = len(preamble_seq)
@@ -45,81 +47,101 @@ def get_window_start(args, wave):
     correlates = []
     real_wave_start_place = -1
     while start + length_preamble <= length:
-        the_wave = wave[start : start + length_preamble]
+        the_wave = wave[start: start + length_preamble]
         correlate = np.dot(the_wave, preamble_seq)
-        if(abs(correlate) >= args.threshold and real_wave_start_place == -1):
+        if abs(correlate) >= args.threshold and real_wave_start_place == -1:
             real_wave_start_place = start
         correlates.append(correlate)
         start += 1
+    plt.plot(correlates)
+    plt.show()
     place = real_wave_start_place + np.argmax(correlates[real_wave_start_place:real_wave_start_place + length_preamble])
     print(place)
     return place
 
 
 def demodulate_one(args, wave):
-    '''
+    """
     描述：获取主导频率来解码一个bit
     参数：全局参数，波---numpy格式的一维数组
     返回：0-1比特
-    '''
+    """
     fourier_result = np.abs(np.fft.fft(wave))
-    #plt.plot(fourier_result)
-    #plt.show()
     length = len(fourier_result)
-    fourier_result = fourier_result[0 : length // 2]
-    max_place = np.argmax(fourier_result)
-    max_frequency = round(max_place * args.framerate / length)
-    dist_0 = abs(max_frequency - args.frequency_0)
-    dist_1 = abs(max_frequency - args.frequency_1)
-    if dist_0 < dist_1:
-        return 0
-    else: 
-        return 1
+    fourier_result = fourier_result[0: length // 2]
+    freq0_place = int(args.frequency_0 * length / args.framerate)
+    freq1_place = int(args.frequency_1 * length / args.framerate)
+    freq0_v = np.max(fourier_result[freq0_place - 10:freq0_place + 10])
+    freq1_v = np.max(fourier_result[freq1_place - 10:freq1_place + 10])
+    return 0 if freq0_v > freq1_v else 1
 
 
 def demodulation(args, wave):
-    '''
+    """
     描述：FSK解调算法
     参数：全局参数，波---numpy格式的一维数组
     返回：0-1比特信号
-    '''
+    """
+    # 绘制短时傅里叶变换图
+    # f, t, zxx = signal.stft(wave, args.framerate)
+    # plt.pcolormesh(t, f, np.abs(zxx))
+    # plt.colorbar()
+    # plt.title('STFT Magnitude')
+    # plt.ylabel('Frequency [Hz]')
+    # plt.xlabel('Time [sec]')
+    # plt.tight_layout()
     length_one = round(args.framerate * args.window_length)
     length = len(wave)
     start = 0
     result = []
-    while(start + length_one <= length):
-        sub_wave = wave[start : start + length_one]
+    while start + length_one <= length:
+        sub_wave = wave[start: start + length_one]
         the_bit = demodulate_one(args, sub_wave)
         result.append(the_bit)
         start += length_one
+    # plt.scatter(np.arange(0, len(result), 1) * 0.01 + 0.005, np.array(result) * 2500 + 7500, marker='o', c='r')
+    # plt.scatter(np.arange(0, len(original_seq), 1) * 0.01 + 0.005, np.array(original_seq) * 2500 + 7600, marker='o', c='b')
+    # plt.show()
     return result
 
-if __name__ == '__main__':
 
-
+def main():
     args = init_args()
-    original_info = '绿罗马帝国强大！1453征服拜占庭！Ceddin deden， neslin baban！'
+    original_info = '北美奴隶种族灭绝反人类匪帮，必须被毁灭，入关！'
     original_seq = string_encode(original_info)
 
-    #TODO:完整的蓝牙包，分包机制
-    original_seq = args.preamble + original_seq
+    # TODO:完整的蓝牙包，分包机制
+    original_seq = np.append(args.preamble, original_seq)
 
-    #get_wave = load_wave(save_base = args.save_base_send, file_name = 'kebab.wav')
-    get_wave = load_wave(save_base = args.save_base_receive, file_name = 'output.wav')
+    # get_wave = load_wave(save_base = args.save_base_send, file_name = 'kebab.wav')
+    get_wave = load_wave(save_base=args.save_base_receive, file_name='o5.wav')
+    get_wave = np.array([float(i[0]) for i in get_wave])
+
+    print("??")
+    print(len(get_wave))
+    plt.plot(get_wave)
+    plt.show()
     place = get_window_start(args, get_wave)
     get_seq = demodulation(args, get_wave[place:])
 
-    #临时代码，到时候应该用蓝牙包解码
-    get_seq = get_seq[ : len(original_seq)]
+    # 临时代码，到时候应该用蓝牙包解码
+    get_seq = get_seq[: len(original_seq)]
 
+    err_count = 0
+    for i in range(len(original_seq)):
+        if get_seq[i] != original_seq[i]:
+            err_count += 1
 
-    #错误率
-    error = np.sum(get_seq != original_seq) / len(original_seq)
+    # 错误率
+    error = err_count / len(original_seq)
     print("{:.4f}%".format(error * 100))
 
-    #TODO：用蓝牙包结构解码
+    # TODO：用蓝牙包结构解码
     packet, place = decode_bluetooth_packet(args, get_seq)
     result = string_decode(packet)
     print(original_info)
     print(result)
-    
+
+
+if __name__ == '__main__':
+    main()
--- src/utils.py
+++ src/utils.py
@@ -8,67 +8,69 @@ import matplotlib.pyplot as plt
 from scipy import signal
 import argparse
 
+
 def init_args():
-    '''
+    """
     描述：加载全局设置---窗口长度，0和1对应的频率，采样频率振幅等其他参数
     参数：无
     返回：args变量，对应全局设置
-    '''
+    """
     parser = argparse.ArgumentParser(description="Choose the parameters")
 
-    #0和1的频率
-    parser.add_argument("--frequency_0", type = int, default = 7500)
-    parser.add_argument("--frequency_1", type = int, default = 10000)
+    # 0和1的频率
+    parser.add_argument("--frequency_0", type=int, default=7500)
+    parser.add_argument("--frequency_1", type=int, default=10000)
 
-    #采样频率，振幅，宽度等通用设置
-    parser.add_argument("--framerate", type = int, default = 48000)
-    parser.add_argument("--sample_width", type = int, default = 2)
-    parser.add_argument("--nchannels", type = int, default = 1)
-    parser.add_argument("--volume", type = float, default = 20000.0)
-    parser.add_argument("--start_place", type = int, default = 0)
+    # 采样频率，振幅，宽度等通用设置
+    parser.add_argument("--framerate", type=int, default=44100)
+    parser.add_argument("--sample_width", type=int, default=2)
+    parser.add_argument("--nchannels", type=int, default=1)
+    parser.add_argument("--volume", type=float, default=20000.0)
+    parser.add_argument("--start_place", type=int, default=0)
 
-    #单个窗口的长度(单位秒)
-    parser.add_argument("--window_length", type = float, default = 0.01)
+    # 单个窗口的长度(单位秒)
+    parser.add_argument("--window_length", type=float, default=0.01)
 
-    #一个包最长长度(多少个比特)
-    parser.add_argument("--packet_length", type = float, default = 1000)
+    # 一个包最长长度(多少个比特)
+    parser.add_argument("--packet_length", type=float, default=1000)
 
-    #保存和接收的文件夹名称
-    parser.add_argument("--save_base_send", type = str, default = 'send')
-    parser.add_argument("--save_base_receive", type = str, default = 'receive')
+    # 保存和接收的文件夹名称
+    parser.add_argument("--save_base_send", type=str, default='send')
+    parser.add_argument("--save_base_receive", type=str, default='receive')
     args = parser.parse_args()
-    
-    #前导码
+
+    # 前导码
     args.preamble = [1, 0, 1, 0, 1, 0, 1, 0]
 
-    #解码用
-    args.threshold = args.volume ** 2
+    # 解码用
+    args.threshold = 1e10  # args.volume ** 2
     return args
 
-def save_wave(my_wave, framerate = 44100, sample_width = 2, nchannels = 1, save_base = 'sound', file_name = 'pulse.wav'):
-    '''
+
+def save_wave(my_wave, framerate=44100, sample_width=2, nchannels=1, save_base='sound', file_name='pulse.wav'):
+    """
     描述：存储wav文件
     参数：波，帧率，采样宽度， 通道数，文件夹名，文件名
     返回：无
-    '''
+    """
     the_place = os.path.join(save_base, file_name)
     wf = wave.open(the_place, 'wb')
     wf.setnchannels(nchannels)
     wf.setframerate(framerate)
     wf.setsampwidth(sample_width)
     for i in range(len(my_wave)):
-        the_result = int(my_wave[i]) 
+        the_result = int(my_wave[i])
         data = struct.pack('<h', the_result)
         wf.writeframesraw(data)
     wf.close()
 
 
-def load_wave(save_base = 'sound', file_name = 'pulse.wav'):
-    '''
+def load_wave(save_base='sound', file_name='pulse.wav'):
+    """
     描述：读取wav文件
     参数：文件夹名，文件名, 帧数，帧率，采样时间，长度
     返回：y
-    '''
+    """
     the_place = os.path.join(save_base, file_name)
     file = wave.open(the_place)
     n_frames = file.getparams().nframes  # 帧总数
@@ -83,27 +85,29 @@ def load_wave(save_base = 'sound', file_name = 'pulse.wav'):
     print("sample_time:", sample_time)
     print("duration:", duration)
     print("frequency:", frequency)
-    y = audio_sequence  
+    y = audio_sequence
     return y
 
+
 def generate_random_seq(length):
-    '''
+    """
     描述：生成随机0-1序列
     参数：长度
     返回：随机0-1序列
-    '''
+    """
     original_seq = []
     for i in range(length):
         num = round(random.random())
         original_seq.append(num)
     return original_seq
 
+
 def compare_seqs(original_seq, get_seq):
-    '''
+    """
     描述：比较两个seq是否一样
     参数：原先和获取的seq
     返回：正确/错误
-    '''
+    """
     if len(original_seq) != len(get_seq):
         return False
     for i in range(len(original_seq)):
@@ -111,13 +115,14 @@ def compare_seqs(original_seq, get_seq):
             return False
     return True
 
+
 def string_encode(string):
-    '''
+    """
     描述：将中英文字符串编码成0-1序列
     参数：中英文混杂的字符串（已经按照大小分包）
     返回：0-1序列数组
-    '''
-    byte_list = str.encode(string, encoding = "utf-8")
+    """
+    byte_list = str.encode(string, encoding="utf-8")
     bit_list = []
     for byte in byte_list:
         for i in range(8):
@@ -125,61 +130,64 @@ def string_encode(string):
             bit_list.append(num)
     return bit_list
 
+
 def string_decode(bit_list):
-    '''
+    """
     描述：将0-1序列解码为中英文字符串
     参数：0-1序列数组（已经去除前导码和包头）
     返回：中英文字符串
-    '''
+    """
     byte_list = []
     length = len(bit_list)
 
-    #补0
+    # 补0
     if length % 8 != 0:
         for i in range(8 - (length % 8)):
             bit_list.append(0)
-    
-    #解析
+
+    # 解析
     start = 0
     while start < len(bit_list):
-        the_byte_list = bit_list[start : start + 8]
+        the_byte_list = bit_list[start: start + 8]
         the_num = 0
         for i in range(8):
             the_num += the_byte_list[i] << (7 - i)
-        #the_byte = chr(the_num)
+        # the_byte = chr(the_num)
         byte_list.append(the_num)
         start += 8
     byte_list = bytes(byte_list)
-    result = bytes.decode(byte_list, encoding = "utf-8")
+    result = bytes.decode(byte_list, encoding="utf-8")
     return result
 
+
 def encode_bluetooth_packet(args, seq):
-    '''
+    """
     TODO：蓝牙包编码
     描述：生成蓝牙包
     参数：全局参数，0-1序列
     返回：完整的蓝牙包(0-1序列)(包括分包)
-    '''
+    """
     return seq
 
+
 def decode_bluetooth_packet(args, packet):
-    '''
+    """
     TODO：蓝牙包解码
     描述：将整个蓝牙包进行拆分
     参数：全局参数，蓝牙包
     返回：经过修正后的内容
-    '''
+    """
     i = 0
     preamble_place = -1
-    #找前导码
+    # 找前导码
     while i < len(packet):
-        if(i + 8 <= len(packet)):
-            the_eight = packet[i : i + 8]
-            if(the_eight == args.preamble):
+        if i + 8 <= len(packet):
+            the_eight = packet[i: i + 8]
+            if the_eight == args.preamble:
                 preamble_place = i
                 break
         i += 1
 
-    the_packet = packet[preamble_place + 8: ]
+    the_packet = packet[preamble_place + 8:]
     the_place = preamble_place
     return the_packet, the_place
